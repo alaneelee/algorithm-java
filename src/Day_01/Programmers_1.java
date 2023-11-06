21e8d957f5f5a86c5e730b31fca0934214cc8ca8
alaneelee <alaneelee@gmail.com>
2023-11-06 21:51:03 +0900

    Day 1

---
+++ src/Day_01/Programmers_1.java
@@ -0,0 +1,12 @@
+package Day_01;
+
+import java.util.Scanner;
+
+public class Programmers_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
---
+++ src/Day_01/Programmers_5.java
@@ -0,0 +1,8 @@
+package Day_01;
+import java.util.Scanner;
+
+public class Programmers_5 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
