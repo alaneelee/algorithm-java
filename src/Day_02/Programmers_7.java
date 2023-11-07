3d5e3e0279e53f41a69c6a288a02006629b6bac3
alaneelee <alaneelee@gmail.com>
2023-11-07 14:37:14 +0900

    Day 2

---
+++ src/Day_02/Programmers_7.java
@@ -0,0 +1,13 @@
+package Day_02;
+
+import java.util.Scanner;
+
+public class Programmers_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a + b);
+    }
+}
