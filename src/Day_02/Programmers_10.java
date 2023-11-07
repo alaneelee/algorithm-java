521e001ea3b26c2b4ec56490df5c3cf3a46c2006
alaneelee <alaneelee@gmail.com>
2023-11-07 14:39:52 +0900

    Day 2

--- src/Day_02/Programmers_10.java
+++ src/Day_02/Programmers_10.java
@@ -1,7 +1,5 @@
 package Day_02;
 
-import java.util.Scanner;
-
 public class Programmers_10 {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = my_string.substring(0,s) + overwrite_string + my_string.substring(s + overwrite_string.length());
