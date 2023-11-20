7e1501c58acb85d5002f0c1271ccd580cabb7960
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-20 12:51:48 +0900

    Day 14 - 수열과 구간 쿼리 1

---
+++ src/Day_14/Programmers_69.java
@@ -0,0 +1,28 @@
+package Day_14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Programmers_69 {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int s = query[0];
+            int e = query[1];
+
+            for (int i = s; i <= e; i++) {
+                arr[i]++;
+            }
+        }
+
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+        Programmers_69 sol = new Programmers_69();
+        int[] arr = {0,1,2,3,4};
+        int[][] queries = {{0,1}, {1,2}, {2,3}};
+        System.out.println(sol.solution(arr, queries));
+    }
+}
+
