66589ba9ff3ddc155f26c475eb9dbad527e3e678
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-22 21:49:14 +0900

    Day 15 - 대문자로 바꾸기

---
+++ src/Day_15/Programmers_75.java
@@ -0,0 +1,13 @@
+package Day_15;
+
+public class Programmers_75 {
+    public String solution(String myString) {
+        return myString.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        Programmers_75 sol = new Programmers_75();
+
+        System.out.println(sol.solution("AbCdEfG"));
+    }
+}
