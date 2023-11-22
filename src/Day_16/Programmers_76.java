875f000c61d07363f7fd07d7912b73ea4119bfb0
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-22 21:50:08 +0900

    Day 15 - 소문자로 바꾸기

---
+++ src/Day_16/Programmers_76.java
@@ -0,0 +1,13 @@
+package Day_16;
+
+public class Programmers_76 {
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        Programmers_76 sol = new Programmers_76();
+
+        System.out.println(sol.solution("AbCdEfG"));
+    }
+}
