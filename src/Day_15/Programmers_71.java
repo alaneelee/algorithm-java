6d2392f20c43936d7c2fa0b6a697f8e474b2cc17
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-22 21:03:12 +0900

    Day 15 - 수열과 구간 쿼리 2

---
+++ src/Day_15/Programmers_71.java
@@ -0,0 +1,31 @@
+package Day_15;
+
+import java.util.Arrays;
+
+public class Programmers_71 {
+    public int solution(int[] arr) {
+        int answer = 0;
+        while (true) {
+            int[] temp = Arrays.copyOf(arr, arr.length);
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] = arr[i] / 2;
+                } else if (arr[i] < 50 && arr[i] % 2 != 0) {
+                    arr[i] = arr[i] * 2 + 1;
+                }
+            }
+            if (Arrays.equals(temp, arr)) {
+                return answer;
+            }
+            answer++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Programmers_71 sol = new Programmers_71();
+        int[] arr = {1, 2, 3, 100, 99, 98};
+        System.out.println(sol.solution(arr));
+    }
+}
+
